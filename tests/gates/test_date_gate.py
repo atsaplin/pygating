34ef3fa5aa457da38f4506013e0cb972a5fab8e1
Aleksey Tsaplin <hernophigus@gmail.com>
2024-01-25 22:33:28 -0500

    fixed date tests to work on github actions

--- tests/gates/test_date_gate.py
+++ tests/gates/test_date_gate.py
@@ -1,7 +1,5 @@
-
 import pytest
 from src.pygating.gates import DateGate
-from src.pygating import GatingException
 from datetime import datetime
 from unittest.mock import Mock
 
@@ -13,19 +11,19 @@ class TestDateGate:
         assert gate._check_gate() == True
 
     # Gate is open when current date is between start_date and end_date
-    def test_gate_open_when_current_date_between_dates(self, monkeypatch):
+    def test_gate_open_when_current_date_between_dates(self):
         start_date = datetime(2022, 1, 1)
         end_date = datetime(2022, 12, 31)
         gate = DateGate(start_date=start_date, end_date=end_date)
 
         gate._get_current_datetime = Mock(return_value=datetime(2022, 6, 5))
-
         assert gate._check_gate() == True
 
     # Gate is open when current date is equal to start_date or end_date
     def test_gate_open_when_current_date_equal_to_dates(self):
-        current_date = datetime.now()
+        current_date = datetime(2022, 6, 5)
         gate = DateGate(start_date=current_date, end_date=current_date)
+        gate._get_current_datetime = Mock(return_value=current_date)
         assert gate._check_gate() == True
 
     # Gate is closed when current date is before start_date and end_date are defined
@@ -48,11 +46,14 @@ class TestDateGate:
 
     # Gate is open when current date is equal to start_date or end_date and allow is False
     def test_gate_open_when_current_date_equal_to_dates_and_allow_false(self):
-        current_date = datetime.now()
+        current_date = datetime(2022, 6, 5)
         gate = DateGate(start_date=current_date, end_date=current_date, allow=False)
+        gate._get_current_datetime = Mock(return_value=current_date)
         assert gate._check_gate() == True
         assert gate.check() == False
 
+
+    # Test from_json with valid data
     def test_from_json_with_valid_data(self):
         json_data = {
             "start_date": "2022-01-01T00:00:00",
@@ -64,6 +65,7 @@ class TestDateGate:
         assert gate.end_date == datetime(2022, 12, 31, 23, 59, 59)
         assert gate.allow == True
 
+    # Test from_json with missing dates
     def test_from_json_with_missing_dates(self):
         json_data = {"allow": False}
         gate = DateGate.from_json(json_data)
@@ -71,6 +73,7 @@ class TestDateGate:
         assert gate.end_date is None
         assert gate.allow == False
 
+    # Test from_json with invalid date format raises ValueError
     def test_from_json_with_invalid_date_format_raises_value_error(self):
         json_data = {
             "start_date": "invalid_date",
